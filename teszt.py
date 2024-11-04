a4158b2aee9f321938514ec99b13438010cc27d6
MarkTothegyetem <marktoth80@gmail.com>
2024-11-04 22:40:10 +0100

    Remove teszt.py

--- teszt.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def count_distinct_values():
-    input = sys.stdin.read
-    data = input().split()[1:]
-    distinct_values = set(data)
-    print(len(distinct_values))
-
-count_distinct_values()
